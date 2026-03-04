d83a4402a77153a87e2a05a44f2c63fe948acfe7
geloxh <ace.blazado7@gmail.com>
2026-03-04 17:16:59 +0800

    User debug

--- backend/models/User.js
+++ backend/models/User.js
@@ -9,12 +9,11 @@ const userSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now }
 });
 
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
+userSchema.pre('save', async function() {
+    if (!this.isModified('password')) return;
     
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
-    next();
 });
 
 
